0615b83ea833e7ddf0a3e0ac07af19aecb7a7202
SIFUENTES MOZOMBITE <1139870@senati.pe>
2019-10-06 14:21:43 -0500

    pagina donde ago pruebas jquery

---
+++ tprue.php
@@ -0,0 +1,93 @@
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- lib  -->
+    <link rel="stylesheet" href="lib/jquery-ui.css">
+    <link rel="stylesheet" href="lib/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="lib/datatables/datatables.min.css">
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.11.4/build/css/alertify.min.css" />
+    <script src="https://kit.fontawesome.com/1bfe247ee7.js" crossorigin="anonymous"></script>
+    <!-- MyStyle -->
+    <link rel="stylesheet" href="view/components/barra/barra.css">
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            border: 0;
+        }
+
+    </style>
+
+</head>
+
+<body>
+<div class="tablas-contenedor table-responsive  container m-4">
+    <table id="tbl-usuario" class="table table-striped table-bordered" style="width:100%">
+    <thead>
+        <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Office</th>
+            <th>Age</th>
+            <th>Start date</th>
+            <th>Salary</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td><i class="fas fa-plus-square"></i><i class="fas fa-minus-square"></i></td>
+            <td>Tiger Nixon</td>
+            <td>System Architect</td>
+            <td>Edinburgh</td>
+            <td>61</td>
+            <td>2011/04/25</td>
+            <td>$320,800</td>
+        </tr>
+        <tr>
+        <td><i class="fas fa-plus-square"></i><i class="fas fa-minus-square"></i></td>
+            <td>Donna Snider</td>
+            <td>Customer Support</td>
+            <td>New York</td>
+            <td>27</td>
+            <td>2011/01/25</td>
+            <td>$112,000</td>
+        </tr>
+    </tbody>
+    <tfoot>
+        <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Office</th>
+            <th>Age</th>
+            <th>Start date</th>
+            <th>Salary</th>
+        </tr>
+    </tfoot>
+
+    </table>
+</div>
+
+    <script src="lib/jquery.js"></script>
+    <script src="lib/jquery-ui.js"></script>
+    <script src="lib/bootstrap/bootstrap.min.js"></script>
+    <script src="lib/bootstrap/bootstrap.bundle.min.js"></script>
+    <script src="lib/datatables/datatables.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/alertifyjs@1.11.4/build/alertify.min.js"></script>
+    <!-- myScript -->
+        <script>
+        $(function () {
+            $('#tlb-usuario tbody tr td').af   
+        
+        });
+        </script>
+</body>
+
+</html>
