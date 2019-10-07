cd53e775e8eebe69f7bd9b1b4691aa2d8cdb8f69
SIFUENTES MOZOMBITE <1139870@senati.pe>
2019-10-06 19:56:08 -0500

    commit actualizado prueba

--- tprue.php
+++ tprue.php
@@ -1,4 +1,3 @@
-
 <!doctype html>
 <html lang="en">
 
@@ -22,59 +21,39 @@
             border: 0;
         }
 
+        #sortable {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            width: 60%;
+        }
+
+        #sortable li {
+            margin: 0 5px 5px 5px;
+            padding: 1em;
+            padding-left: 1.5em;
+            font-size: 1em;
+            height: 18px;
+        }
+
+        #sortable li span {
+            position: absolute;
+            margin-left: -1.3em;
+        }
     </style>
 
 </head>
 
 <body>
-<div class="tablas-contenedor table-responsive  container m-4">
-    <table id="tbl-usuario" class="table table-striped table-bordered" style="width:100%">
-    <thead>
-        <tr>
-            <th></th>
-            <th>Name</th>
-            <th>Position</th>
-            <th>Office</th>
-            <th>Age</th>
-            <th>Start date</th>
-            <th>Salary</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td><i class="fas fa-plus-square"></i><i class="fas fa-minus-square"></i></td>
-            <td>Tiger Nixon</td>
-            <td>System Architect</td>
-            <td>Edinburgh</td>
-            <td>61</td>
-            <td>2011/04/25</td>
-            <td>$320,800</td>
-        </tr>
-        <tr>
-        <td><i class="fas fa-plus-square"></i><i class="fas fa-minus-square"></i></td>
-            <td>Donna Snider</td>
-            <td>Customer Support</td>
-            <td>New York</td>
-            <td>27</td>
-            <td>2011/01/25</td>
-            <td>$112,000</td>
-        </tr>
-    </tbody>
-    <tfoot>
-        <tr>
-            <th></th>
-            <th>Name</th>
-            <th>Position</th>
-            <th>Office</th>
-            <th>Age</th>
-            <th>Start date</th>
-            <th>Salary</th>
-        </tr>
-    </tfoot>
-
-    </table>
-</div>
-
+    <ul id="sortable">
+        <li class="ui-state-default"><span class="fas fa-plus-square"></span>Item 1</li>
+        <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 2</li>
+        <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 3</li>
+        <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 4</li>
+        <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 5</li>
+        <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 6</li>
+        <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 7</li>
+    </ul>
     <script src="lib/jquery.js"></script>
     <script src="lib/jquery-ui.js"></script>
     <script src="lib/bootstrap/bootstrap.min.js"></script>
@@ -82,12 +61,14 @@
     <script src="lib/datatables/datatables.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/alertifyjs@1.11.4/build/alertify.min.js"></script>
     <!-- myScript -->
-        <script>
-        $(function () {
-            $('#tlb-usuario tbody tr td').af   
-        
+    <script>
+        $(function() {
+
+            $("#sortable").sortable();
+            $("#sortable").disableSelection();
+
         });
-        </script>
+    </script>
 </body>
 
 </html>
