9100c5f758cb0559e1b2391d33b4f22edfd9e881
SIFUENTES MOZOMBITE <1139870@senati.pe>
2019-10-08 14:49:19 -0500

    commit leonardo

--- tprue.php
+++ tprue.php
@@ -48,9 +48,6 @@
     <ul id="sortable">
 <<<<<<< HEAD
         <li class="ui-state-default"><span class="fas fa-plus-square"></span>Item 1saasd</li>
-        <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 9</li>
-        <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 4</li>
-=======
         <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 5</li>
         <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 6</li>
         <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 7</li>
