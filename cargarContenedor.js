284b52d606e272239cca86793cbcc502bdc832d3
teamLeonardo <leonardosm3.14@gmail.com>
2019-10-17 16:08:00 -0500

    control

--- cargarContenedor.js
+++ cargarContenedor.js
@@ -3,15 +3,21 @@ $(document).ready(function() {
     $(".vertical-nav-menu").on('click', 'a', function(e) {
         var $seleccion = $(this).attr('id');
         $(this).closest('.vertical-nav-menu').find('a.mm-active').removeClass('mm-active');
-
-
         if ($seleccion == undefined) {
             console.log('no tiene redirecion');
         } else {
             $(this).addClass('mm-active');
             console.log($seleccion);
             var modulo = $(this).closest('li.mm-active').attr('id');
-            $contenedor.load("view/components/" + modulo + "/" + $seleccion + ".php");
+            if (modulo != undefined) {
+
+                $contenedor.load("view/components/" + modulo + "/" + $seleccion + ".php");
+            } else {
+                modulo = $(this).closest('li').attr('id');
+                $contenedor.load("view/components/" + modulo + "/" + $seleccion + ".php");
+
+            }
+
         }
         e.preventDefault();
     });
--- view/components/main/main.php
+++ view/components/main/main.php
@@ -33,19 +33,19 @@
     <div class="scrollbar-sidebar">
         <div class="app-sidebar__inner">
             <ul class="vertical-nav-menu">
-                <li class="app-sidebar__heading">inicio</li>
-                <li>
-                    <a id="inicio">
+                <li class="app-sidebar__heading" >inicio</li>
+                <li id="inicio">
+                    <a id="primeraVista">
                         <i class="metismenu-icon pe-7s-diamond"></i>control principal
                     </a>
                 </li>
                 <li class="app-sidebar__heading">Modulos</li>
-                <li id="ventas" class="mm-active" >
+                <li id="ventas"  >
                     <a href="">
                         <i class="metismenu-icon pe-7s-calculator"></i>Modulo de Ventas
                         <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
                     </a>
-                    <ul id="modulo-sub-ventas" class="mm-show">
+                    <ul id="modulo-sub-ventas" >
                         <li>
                             <a href="" id="registrar_cliente" class="mm-active">
                                 <i class="metismenu-icon" ></i>Registro de Cliente
